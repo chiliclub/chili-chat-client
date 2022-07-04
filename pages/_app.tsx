4b66d4dba54415af35012ea15f4963634380faab
steadily-worked <iamsangminpark@gmail.com>
2022-07-04 17:34:20 +0900

    feat: add sample pages and component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "styles/globalStyle";
+import { theme } from "styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>chiliclub</title>
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
---
+++ pages/sample/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SampleComponent from "@components/Sample";
+
+const SamplePages = () => {
+  return <SampleComponent />;
+};
+
+export default SamplePages;
