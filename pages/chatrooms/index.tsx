654279308da58ac7996b32d379dce867cd2e9166
Sangmin Park <iamsangminpark@gmail.com>
2022-08-14 21:23:44 +0900

    implement: markup chatroom list

--- pages/chatrooms/index.tsx
+++ pages/chatrooms/index.tsx
@@ -2,11 +2,14 @@ import { fetchChatroomsList } from "@api/chatrooms";
 import NewChatroomModal from "@components/Chatrooms/NewChatroomModal";
 import { Title, FormWrapper, Button } from "@styles/commonStyles";
 import { ChatroomsListType } from "@type/ChatroomsList";
+import { dateParse } from "@utils/dateParse";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 
 const ChatRoomsPage = () => {
+  const router = useRouter();
   const { data: chatroomsListData } = useQuery<Array<ChatroomsListType>>(
     ["chatroomsList"],
     () => fetchChatroomsList(),
@@ -24,22 +27,80 @@ const ChatRoomsPage = () => {
         <NewChatroomModal onClose={() => setIsNewChatroomModalOpen(false)} />
       )}
       <Title>채팅방 목록</Title>
-      <ChatroomListWrapper>
-        {chatroomsListData?.map(
-          (eachChatroom: ChatroomsListType, idx: number) => {
-            return <div key={idx}>{eachChatroom.author}</div>;
-          }
-        )}
-      </ChatroomListWrapper>
       <ButtonContainer>
         <Button onClick={() => setIsNewChatroomModalOpen(true)}>
           채팅방 만들기
         </Button>
         <Button>내 프로필 보기</Button>
       </ButtonContainer>
+      <ChatroomListWrapper>
+        {chatroomsListData?.map(
+          (eachChatroom: ChatroomsListType, idx: number) => {
+            return (
+              <Container
+                key={eachChatroom.chatRoomNo}
+                onClick={() =>
+                  router.push(`/chatrooms/${eachChatroom.chatRoomNo}`)
+                }
+              >
+                <MainSection>
+                  <ChatRoomTitle>{eachChatroom.title}</ChatRoomTitle>
+                  <AdminUser>
+                    총 참여자수: {eachChatroom.totalParticipantCount}
+                  </AdminUser>
+                </MainSection>
+
+                <SubSection>
+                  {eachChatroom.updDatetime && (
+                    <UpdDatetime>
+                      최종 업데이트: {dateParse(eachChatroom.updDatetime)}
+                    </UpdDatetime>
+                  )}
+                  <AdminUser>방장: {eachChatroom.adminUserNo}</AdminUser>
+                </SubSection>
+              </Container>
+            );
+          }
+        )}
+      </ChatroomListWrapper>
     </FormWrapper>
   );
 };
+
+const Container = styled.div`
+  border: 1px solid black;
+  width: 15rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ChatRoomTitle = styled.h1`
+  width: 10rem;
+`;
+
+const MainSection = styled.section`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const SubSection = styled.section`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const UpdDatetime = styled.h5`
+  margin: 0;
+`;
+
+const AdminUser = styled.h4`
+  margin: 0;
+`;
 
 const ChatroomListWrapper = styled.section`
   display: flex;
--- type/ChatroomsList.ts
+++ type/ChatroomsList.ts
@@ -2,11 +2,9 @@ export type ChatroomsListType = {
   insDatetime: string;
   updDatetime: string;
   title: string;
-  author: string;
-  totalCount: number;
+  adminUserNo: string;
+  totalParticipantCount: number;
+  chatRoomNo: number;
 };
 
-export type NewChatroomType = Omit<
-  ChatroomsListType,
-  "insDatetime" | "updDatetime" | "author" | "totalCount"
->;
+export type NewChatroomType = Pick<ChatroomsListType, "title">;
